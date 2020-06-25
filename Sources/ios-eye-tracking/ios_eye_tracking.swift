5cb3f52f14b281c412867914b7684f9a4fbc731a
Kyle Fox <kefox@pbs.org>
2020-06-25 12:24:35 -0400

    Advice from ARKit engineer on converting to screen coordinates!

--- Sources/ios-eye-tracking/ios_eye_tracking.swift
+++ Sources/ios-eye-tracking/ios_eye_tracking.swift
@@ -38,12 +38,33 @@ public class EyeTracking: NSObject {
 extension EyeTracking: ARSessionDelegate {
     public func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
         guard let anchor = anchors.first as? ARFaceAnchor else { return }
-        print("⛔️ \(anchor.lookAtPoint)")
+
+//        print("⛔️ \(anchor.lookAtPoint)")
+
         // Convert to screen coordinates.
 
-        print("⛔️ \(String(describing: anchor.blendShapes[.eyeBlinkLeft]))")
-        print("⛔️ \(String(describing: anchor.blendShapes[.eyeBlinkRight]))")
+        let faceAnchor = anchors.first
+
+        let point = faceAnchor!.transform * SIMD4<Float>(anchor.lookAtPoint, 1)
+
+
+
+//        print("⛔️ \(String(describing: anchor.blendShapes[.eyeBlinkLeft]))")
+//        print("⛔️ \(String(describing: anchor.blendShapes[.eyeBlinkRight]))")
+
         // Determine whether the user has blinked.
+
+    }
+
+    public func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        guard let anchor = frame.anchors.first as? ARFaceAnchor else { return }
+
+        let point = anchor.transform * SIMD4<Float>(anchor.lookAtPoint, 1)
+
+        let screenPoint = frame.camera.projectPoint(SIMD3<Float>(x: point.x, y: point.y, z: point.z), orientation: .landscapeLeft, viewportSize: UIScreen.main.bounds.size)
+        print("⛔️ \(screenPoint)")
+
+
     }
 }
 
