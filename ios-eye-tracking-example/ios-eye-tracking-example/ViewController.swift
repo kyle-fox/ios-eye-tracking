e719d2110d0eaac7bcb51a9f548a7808d0d524d8
Kyle Fox <kefox@pbs.org>
2020-07-24 19:00:23 -0400

    Adding some demos to the example app

--- ios-eye-tracking-example/ios-eye-tracking-example/ViewController.swift
+++ ios-eye-tracking-example/ios-eye-tracking-example/ViewController.swift
@@ -3,23 +3,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let eyeTracking = EyeTracking(configuration: Configuration(appID: "ios-eye-tracking-example", blendShapes: [.eyeBlinkLeft, .eyeBlinkRight]))
+    let eyeTracking = EyeTracking(configuration: Configuration(appID: "ios-eye-tracking-example"))
     var sessionID: String?
+    var sessionTimer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        eyeTracking.startSession()
-        eyeTracking.loggingEnabled = true
-        sessionID = eyeTracking.currentSession?.id
+        startNewSession()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         eyeTracking.showPointer()
+    }
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) {
-            self.eyeTracking.endSession()
-            EyeTracking.displayScanpath(for: self.sessionID ?? "", animated: true)
+    func startNewSession() {
+        // Only run 1 session's data at a time
+        try? EyeTracking.deleteAll()
+
+        if eyeTracking.currentSession != nil {
+            eyeTracking.endSession()
         }
+
+        eyeTracking.startSession()
+        eyeTracking.loggingEnabled = true
+        sessionID = eyeTracking.currentSession?.id
+    }
+
+    @IBAction func startNewSessionTapped(_ sender: Any) {
+        startNewSession()
+    }
+
+    @IBAction func startDataSession(_ sender: Any) {
+        startNewSession()
+
+        sessionTimer?.invalidate()
+        sessionTimer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: false) { _ in
+            self.eyeTracking.loggingEnabled = false
+            self.eyeTracking.endSession()
+            guard let jsonData = try? EyeTracking.exportAll() else { return }
+            let jsonDict = try? JSONSerialization.jsonObject(with: jsonData, options: .allowFragments)
+            print(jsonDict ?? "")
+            self.eyeTracking.startSession()
+        }
+    }
+
+    @IBAction func startScanpathSession(_ sender: Any) {
+        startNewSession()
+
+        sessionTimer?.invalidate()
+        sessionTimer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: false) { _ in
+            self.eyeTracking.endSession()
+            self.eyeTracking.displayScanpath(for: self.sessionID ?? "", animated: true)
+        }
+    }
+
+    @IBAction func hideScanpath(_ sender: Any) {
+        eyeTracking.hideVisualization()
     }
 }
